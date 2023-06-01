bdb63250ef21f265744743dafcf2e2e96ed6c541
rohan1126 <rohanagni26@gmail.com>
2023-06-01 13:19:35 -0700

    added stuff

---
+++ my-flask-app/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, jsonify
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/currently-playing')
+@app.route('/currently-playing')
+def currently_playing():
+    scope = "user-read-currently-playing"
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id='a5f6ed27ca2b416daa27d5e39f5f5877',
+                                                  client_secret='a5ae4fd03e1f4d0ea1120a4d7d0d4c4c',
+                                                  redirect_uri='http://localhost:5000/callback'))
+    current_track = sp.current_user_playing_track()
+    if current_track is not None and current_track['is_playing']:
+        track_name = current_track['item']['name']
+        artist_name = current_track['item']['artists'][0]['name']
+        album_name = current_track['item']['album']['name']
+        album_cover_url = current_track['item']['album']['images'][0]['url']
+        progress_ms = current_track['progress_ms']
+        duration_ms = current_track['item']['duration_ms']
+        return jsonify(track_name=track_name, artist_name=artist_name,
+                       album_name=album_name, album_cover_url=album_cover_url,
+                       progress_ms=progress_ms, duration_ms=duration_ms)
+
+    else:
+        return jsonify(track_name='No track currently playing')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
